01412a77f0edd3e572d2a4dc14a9c1a0c2465809
Willian Motta Ferreira Braga <73251848+WillianMotta@users.noreply.github.com>
2021-10-28 21:45:18 -0300

    atualização do slider

--- projeto front/script.js
+++ projeto front/script.js
@@ -1,81 +1,30 @@
+/*
+  this was made thanks to Wes Bos's video:
+  https://www.youtube.com/watch?v=C9EWifQ5xqA
+*/
+const lists = document.querySelectorAll('.movie-list');
+let isDown = false;
+let StartX, scrollLeft;
 
-$(document).ready(function() {
-var $imagesCarousel = $('.carouselOfImages').flickity({
-  contain: true,
-  autoPlay: true,
-  wrapAround: true,
-  friction: 0.3
-});
-
-
-function resizeCells() {
-  var flkty = $imagesCarousel.data('flickity');
-  var $current = flkty.selectedIndex
-  var $length = flkty.cells.length
-  // if ($length <='0') {
-  //   $imagesCarousel.flickity('destroy');
-  // }
-  $('.carouselOfImages .carouselImage').removeClass("nextToSelected");
-  $('.carouselOfImages .carouselImage').eq($current-1).addClass("nextToSelected");
-  if ($current+1 == $length) {
-    var $endCell = "0"
-  } else {
-    var $endCell = $current+1
-  }
-   $('.carouselOfImages .carouselImage').eq($endCell).addClass("nextToSelected");
-  };
-resizeCells();
-
-$imagesCarousel.on('scroll.flickity', function() {
-    resizeCells();
-  });
-  
-  
-  
-  
-  
-$(".carouselImage img").click(function() { 
-  var $this = $(this);
-  var imageID = $this.attr('data-tab');
-  var imageSrc = $this.attr('src');
-  
-  $('.' + imageID).removeClass('hide');
-  $('.' + imageID + ' .product-detail-image img').attr('src', imageSrc);
-});
-
-$('.product-detail-close,.product-detail').on('click', function() {
-  $('.product-detail').addClass('hide');
-});
-
-  $('.modal-video').on('hidden.bs.modal', function (e) {
-    $('.modal-video iframe').attr('src', $('.modal-video iframe').attr('src'));
-  });
-
-autoPlayYouTubeModal();
-
-  function autoPlayYouTubeModal() {
-      var trigger = $("body").find('[data-the-video]');
-      trigger.click(function () {
-          var theModal = $(this).data("target"),
-              videoSRC = $(this).attr("data-the-video"),
-              videoSRCauto = videoSRC + "&autoplay=1";
-          $(theModal + ' iframe').attr('src', videoSRCauto);
-          $(theModal + ' button.close').click(function () {
-              $(theModal + ' iframe').attr('src', videoSRC);
-          });
-          $('.modal-video').click(function () {
-              $(theModal + ' iframe').attr('src', videoSRC);
-          });
-      });
-  }
-
-$(window).on('load resize', function(){
-  var $window = $(window);
-  $('.modal-fill-vert .modal-body > *').height(function(){
-      return $window.height()-60;
+lists.forEach(list => {
+    list.addEventListener('mousedown', (e) => {
+        isDown = true;
+        StartX = e.pageX - list.offsetLeft;
+        scrollLeft = list.scrollLeft;
     });
-  }); 
+    list.addEventListener('mouseleave', () => {
+        isDown = false;
+    });
+    list.addEventListener('mouseup', () => {
+        isDown = false;
+    });
+    list.addEventListener('mousemove', (e) => {
+        if(!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - list.offsetLeft;
+        const walk = x - StartX;
+        list.scrollLeft = scrollLeft - walk;
+    });
+    
 });
-
-
 
